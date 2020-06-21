829bcdae8db2ff5602f3cea9831ef35b8458be11
soundznice <andreydan3@gmail.com>
2020-06-21 03:39:25 +0300

    Fixed artists

--- src/MusicUniverse.Persistence/Migrations/20200621000655_composition_albumv2.Designer.cs
+++ src/MusicUniverse.Persistence/Migrations/20200621000655_composition_albumv2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicUniverse.Persistence;
 
 namespace MusicUniverse.Persistence.Migrations
 {
     [DbContext(typeof(MusicUniverseDbContext))]
-    partial class MusicUniverseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200621000655_composition_albumv2")]
+    partial class composition_albumv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -269,7 +271,7 @@ namespace MusicUniverse.Persistence.Migrations
                         .HasForeignKey("AlbumId");
 
                     b.HasOne("MusicUniverse.Domain.Entities.Artist", "Artist")
-                        .WithMany("Compositions")
+                        .WithMany()
                         .HasForeignKey("ArtistId1");
 
                     b.HasOne("MusicUniverse.Domain.Entities.Image", "Image")
---
+++ src/MusicUniverse.Persistence/Migrations/20200621000655_composition_albumv2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicUniverse.Persistence.Migrations
+{
+    public partial class composition_albumv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ImageId = table.Column<int>(nullable: true),
+                    ArtistId = table.Column<int>(nullable: true),
+                    ArtistId1 = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Albums_Artists_ArtistId1",
+                        column: x => x.ArtistId1,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Albums_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compositions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ArtistId = table.Column<int>(nullable: true),
+                    AlbumId = table.Column<int>(nullable: true),
+                    ImageId = table.Column<int>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    Lyrics = table.Column<string>(nullable: true),
+                    ArtistId1 = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compositions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Compositions_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Compositions_Artists_ArtistId1",
+                        column: x => x.ArtistId1,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Compositions_Images_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Images",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_ArtistId1",
+                table: "Albums",
+                column: "ArtistId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Albums_ImageId",
+                table: "Albums",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compositions_AlbumId",
+                table: "Compositions",
+                column: "AlbumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compositions_ArtistId1",
+                table: "Compositions",
+                column: "ArtistId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compositions_ImageId",
+                table: "Compositions",
+                column: "ImageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compositions");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
